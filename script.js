92498788eb63702ff879135c51702a09938e7b39
KatrinMaria <katrinmaria111@gmail.com>
2021-01-05 00:53:31 +0000

    javaScript

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+//Når siden er loaded
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("sidenVises");
+
+
+    document.querySelector("#box_1").classList = "box pattern";
+
+}
+
 const knap = document.querySelector('#knap');
 const box = document.querySelector('nav');
 
 knap.addEventListener('click', toggle);
 
 function toggle() {
+    console.log("toggle");
     box.classList.toggle('toggle');
+    document.querySelector("#box_1").classList = "paused forsvind";
 }
