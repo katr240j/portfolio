6024fd52fcc6463ee25d949681227bd0ba3f0624
KatrinMaria <katrinmaria111@gmail.com>
2021-01-28 02:52:07 +0100

    jeminn einasti

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-//Når siden er loaded
-window.addEventListener("load", sidenVises);
-
-
-function sidenVises() {
-    console.log("sidenVises");
-
-}
-
 const knap = document.querySelector('#knap');
 const box = document.querySelector('nav');
-const box_1 = document.querySelector('#box_1');
 
 knap.addEventListener('click', toggle);
 
@@ -17,14 +7,6 @@ function toggle() {
     console.log("toggle");
     box.classList.toggle('toggle');
 
-    box_1.classList = "paused forsvind";
-    /*
-        if (box_1.classlist.contains('paused')) {
-            console.log("er pasad")
-            // document.querySelector("#box_1").classListRemove = "paused forsvind";
-        } else
-            console.log("ekki pasad")
-        // document.querySelector("#box_1").classList = "paused forsvind";
-    */
+
 
 }
