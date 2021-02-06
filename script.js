b5675a7e2e4d7b2a36b7a8c55f6809c48829d9c3
KatrinMaria <katrinmaria111@gmail.com>
2021-02-06 18:52:21 +0100

    script lagað

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const knap = document.querySelector('#knap');
-const box = document.querySelector('nav');
+const knap = document.querySelector("#knap");
+const box = document.querySelector("nav");
 
 knap.addEventListener('click', toggle);
 
@@ -7,6 +7,12 @@ function toggle() {
     console.log("toggle");
     box.classList.toggle('toggle');
 
-
+    let erSkjult = box.classList.contains('toggle');
+    if (erSkjult == true) {
+        knap.textContent = "☰";
+        //hvis der er rigtigt så er der en synlig burgermenu
+    } else {
+        knap.textContent = "X";
+    }
 
 }
